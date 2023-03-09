2cd333b45ce04f17e775c19aa0dd3e7c2f4b19c2
David Sales <121496342+DavSalles@users.noreply.github.com>
2023-03-09 20:23:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let ema = prompt("Você ama Emannuel?");  
+
+if (ema == "sim") {
+    alert("Resposta certa");
+} else  {
+    alert("Resposta errada, sua falsa");
+}
+
+
+    const namorar = prompt("Voce gosta de namorar Emannuel?") 
+     namorar = sim
+     if (namorar == sim) {
+        window.alert("Você esta deixando Emannuel muito feliz sr !!!")
+    } else {
+        alert('te odeio')
+    }
